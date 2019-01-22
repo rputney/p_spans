77c251ec62fa769ace7bcdf5268cfd2d4cafee22
rputney <ryanputney@gmail.com>
2019-01-22 11:55:50 -0500

    Add files via upload

---
+++ p_spans.R
@@ -0,0 +1,374 @@
+
+find.sig.sigletons <- function(){
+}
+
+p.q.mean.spans <- function(p.q.vals, abs.mean.diff, p.q.thresh, mean.diff.thresh, span, promote.singles){
+
+    below <- p.q.vals <= p.q.thresh & abs.mean.diff >= mean.diff.thresh
+	if(promote.singles & length(below) > 2) below <- below | (c(1,below[1:(length(below)-1)]) & c(below[2:length(below)],1))
+	below <- c(FALSE, below, FALSE)
+	d <- diff(below)
+	rising <- which(d == 1)
+	falling <- which(d == -1)
+	width <- falling - rising
+	sat.span <- width >= span
+	start.pos <- rising[sat.span]
+	end.pos <- falling[sat.span]-1
+	cbind(start.pos, end.pos, width=end.pos-start.pos+1)
+}
+
+p.q.mean.spans2 <- function(p.q.vals, abs.mean.diff, singleton, p.q.thresh, mean.diff.thresh, span, promote.singles){
+    below <- p.q.vals <= p.q.thresh & abs.mean.diff >= mean.diff.thresh
+    #if (length(below) == 0){
+    #    return(NULL)
+    #}
+	if(promote.singles & length(below) > 2) below <- below | (c(1,below[1:(length(below)-1)]) & c(below[2:length(below)],1))
+    below <- below & !singleton
+	below <- c(FALSE, below, FALSE)
+	d <- diff(below)
+	rising <- which(d == 1)
+	falling <- which(d == -1)
+	width <- falling - rising
+	sat.span <- width >= span
+	start.pos <- rising[sat.span]
+	end.pos <- falling[sat.span]-1
+	cbind(start.pos, end.pos, width=end.pos-start.pos+1)
+}
+
+p.spans <- function(x, threshold = 0.05, span = 2, allow.singles = TRUE){
+
+	below <- x <= threshold
+	if(!allow.singles) below <- below | (c(1,below[1:(length(below)-1)]) & c(below[2:length(below)],1))
+	below <- c(FALSE, below, FALSE)
+	d <- diff(below)
+	rising <- which(d == 1)
+	falling <- which(d == -1)
+	width <- falling - rising
+	sat.span <- width >= span
+	start.pos <- rising[sat.span]
+	end.pos <- falling[sat.span]-1
+	cbind(start.pos, end.pos, width=end.pos-start.pos+1)
+}
+
+p.mean.spans <- function(t.table, p.thresh=0.001, mean.diff.thresh=0.3, span = 3, promote.singles = TRUE){
+
+    pvals <- t.table$p.value
+    mean.diff <- t.table$abs.mean.diff
+	below <- pvals <= p.thresh & mean.diff >= mean.diff.thresh
+	if(promote.singles) below <- below | (c(1,below[1:(length(below)-1)]) & c(below[2:length(below)],1))
+	below <- c(FALSE, below, FALSE)
+	d <- diff(below)
+	rising <- which(d == 1)
+	falling <- which(d == -1)
+	width <- falling - rising
+	sat.span <- width >= span
+	start.pos <- rising[sat.span]
+	end.pos <- falling[sat.span]-1
+	cbind(start.pos, end.pos, width=end.pos-start.pos+1)
+}
+
+q.mean.spans <- function(t.table, q.thresh=0.01, mean.diff.thresh=0.3, span = 2, promote.singles = TRUE){
+    qvals <- t.table$q.value
+    mean.diff <- t.table$abs.mean.diff
+	below <- qvals <= q.thresh & mean.diff >= mean.diff.thresh
+	if(promote.singles) below <- below | (c(1,below[1:(length(below)-1)]) & c(below[2:length(below)],1))
+	below <- c(FALSE, below, FALSE)
+	d <- diff(below)
+	rising <- which(d == 1)
+	falling <- which(d == -1)
+	width <- falling - rising
+	sat.span <- width >= span
+	start.pos <- rising[sat.span]
+	end.pos <- falling[sat.span]-1
+	cbind(start.pos, end.pos, width=end.pos-start.pos+1)
+}
+
+t.test.list2table <- function(tList){
+    require(qvalue)
+	m <- sapply(tList, function(test) {
+	                                   means <- unname(test$estimate)
+									   c(x.mean=means[1], y.means=means[2],
+									     abs.mean.diff=abs(means[2]-means[1]),
+										 mean.diff=means[2]-means[1],
+										 p.value=test$p.value)
+										})
+	m <- as.data.frame(t(m))
+	m$q.value <- qvalue(m$p.value)$qvalues
+	m
+}
+
+my.dmp.finder <- function(GenomicMethylSet, group.pos, group.neg, rm.na = TRUE, offset = 100){
+    require(qvalue)
+    betas <- getBeta(GenomicMethylSet, offset = offset)
+    if (rm.na) {
+        betas <- na.omit(betas)
+    }
+    anno <- getAnnotation(GenomicMethylSet)[rownames(betas),]
+    locs <- granges(GenomicMethylSet)[rownames(betas)]
+    mcols(locs) <- anno[, c(4, 1:2)]
+    locs$str <- anno$strand
+    locs$dist.to.next <- distance(locs, c(locs[-1], locs[1]))
+    locs$dist.to.prev <- distance(locs, c(locs[length(locs)], locs[-length(locs)]))
+    locs$singleton <- (locs$dist.to.next > 500 | is.na(locs$dist.to.next)) & (locs$dist.to.prev > 500 | is.na(locs$dist.to.prev))
+    mcols(locs) <- cbind(mcols(locs), anno[,c(19,18,24,26,25,32)])
+    t.list <- lapply(1:nrow(betas), function(r) {
+                                                 my.t <- t.test(x = betas[r,group.neg], y = betas[r,group.pos], alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95)
+                                                 means <- unname(my.t$estimate)
+                                                 c(group.pos.mean=means[2], group.neg.means=means[1],
+                                                 abs.mean.diff=abs(means[2]-means[1]),
+                                                 mean.diff=means[2]-means[1],
+                                                 p.value=my.t$p.value)
+                                                })
+    t.df<- as.data.frame(do.call("rbind", t.list))
+    t.df$q.value <- qvalue(t.df[,"p.value"])$qvalues
+    mcols(locs) <- cbind(mcols(locs), t.df)
+    locs
+}
+
+
+my.dmr.finder <- function(dmp.table, use.value.dmr=c("p", "q"), p.q.thresh.dmr=0.01, mean.diff.thresh.dmr=0.3, span=3, promote.singles=TRUE,
+                          find.singletons=TRUE, use.value.sing=NULL, p.q.thresh.sing=NULL, mean.diff.thresh.sing=NULL, merge.tables=FALSE) {
+    require(GenomicRanges)
+    use.value.dmr <- match.arg(use.value.dmr)
+    if(is.null(use.value.sing)) use.value.sing <- use.value.dmr
+    else use.value.sing <- match.arg(use.value.dmr)
+    if(is.null(p.q.thresh.sing)) p.q.thresh.sing <- p.q.thresh.dmr
+    if(is.null(mean.diff.thresh.sing)) mean.diff.thresh.sing <- mean.diff.thresh.dmr
+    if(!find.singletons) merge.tables <- FALSE
+    
+    #mcols.slice <- c(20:21,1:4,8:13,17:19,22:23)
+    mcols.slice <- c(20:21,1:4,8:13,17:19,22:23,7)
+    #dmp.glist <- split(dmp.table, factor(as.vector(seqnames(dmp.table))))
+    dmp.glist <- split(dmp.table, seqnames(dmp.table))
+#return(dmp.glist)
+    dmr.list <- lapply(dmp.glist, function(x) {
+                                            if(use.value.dmr == "p") {
+                                                spans <- p.q.mean.spans2(x$p.value, x$abs.mean.diff, x$singleton, p.q.thresh.dmr, mean.diff.thresh.dmr, span, promote.singles)
+                                            } else {
+                                                spans <- p.q.mean.spans2(x$q.value, x$abs.mean.diff, x$singleton, p.q.thresh.dmr, mean.diff.thresh.dmr, span, promote.singles)
+                                            }
+                                            if(nrow(spans) > 0) {
+                                                do.call("c", lapply(1:nrow(spans), function(y) {
+                                                                    r <- spans[y,]
+                                                                    g.slice <- x[r[1]:r[2],]
+                                                                    range.w.prev <- c(0, diff(g.slice$pos)-1)
+                                                                    dmr.range <- g.slice$pos[length(g.slice$pos)] - g.slice$pos[1] - 1
+                                                                    g.slice$window.num <- y
+                                                                    g.slice$window.size <- r[3]
+                                                                    g.slice$width.to.prev <- range.w.prev
+                                                                    g.slice$dmr.width <- dmr.range
+                                                                    g.slice[, mcols.slice]
+                                                                  }))
+                                            }
+                                            })
+
+    if(find.singletons) { 
+        singleton.list <- lapply(dmp.glist, function(xx) {
+                                            if(use.value.sing == "p") {
+                                                sig.singletons <- xx$singleton & xx$p.value <= p.q.thresh.sing & xx$abs.mean.diff >= mean.diff.thresh.sing
+                                            } else {
+                                                sig.singletons <- xx$singleton & xx$q.value <= p.q.thresh.sing & xx$abs.mean.diff >= mean.diff.thresh.sing
+                                            }
+                                            sig.singletons.spans <- cbind(start=which(sig.singletons), end=which(sig.singletons), width=rep(1,sum(sig.singletons)))
+                                            if(nrow(sig.singletons.spans) > 0) {
+                                                do.call("c", lapply(1:nrow(sig.singletons.spans), function(yy) {
+                                                                    r <- sig.singletons.spans[yy,1]
+                                                                    g.slice <- xx[r,]
+                                                                    g.slice$window.num <- yy
+                                                                    g.slice$window.size <- 1
+                                                                    g.slice$width.to.prev <- 0
+                                                                    g.slice$dmr.width <- 0
+                                                                    g.slice[, mcols.slice]
+                                                                    
+                                                                  }))
+                                            }
+                                            })
+    }
+#return(dmr.list)
+    dmr.granges <- unlist(GRangesList(dmr.list[!sapply(dmr.list, is.null)]), use.names=FALSE)
+    if(find.singletons) {
+        singleton.granges <- unlist(GRangesList(singleton.list[!sapply(singleton.list, is.null)]), use.names=FALSE)
+    } else {
+        singleton.granges <- NULL
+    }
+    if(!merge.tables) {
+        if(!is.null(dmr.granges)) {
+            win.num <- sum(dmr.granges$width.to.prev == 0)
+            dmr.granges$window.num <- rep(1:win.num, diff(c(which(dmr.granges$width.to.prev == 0), length(dmr.granges$width.to.prev)+1)))
+        }
+        if(!is.null(singleton.granges)) {
+            singleton.granges$window.num <- 1:length(singleton.granges)
+        }
+        if(find.singletons) {
+            return(list(dmr.granges=dmr.granges, singleton.granges=singleton.granges))
+        } else {
+            return(dmr.granges)
+        }
+    } else {
+        if(!is.null(dmr.granges) && !is.null(singleton.granges)) {
+            all.granges <- sort(c(dmr.granges, singleton.granges))
+            #all.granges <- all.granges[order(all.granges$chr, all.granges$pos)]
+            win.num <- sum(all.granges$width.to.prev == 0)
+            all.granges$window.num <- rep(1:win.num, diff(c(which(all.granges$width.to.prev == 0), length(all.granges$width.to.prev)+1)))
+        } else if (!is.null(singleton.granges)) {
+            all.granges <- singleton.granges
+        } else {
+            all.granges <- dmr.granges
+        }
+        return(all.granges)
+        if(!is.null(all.granges)) {
+            all.granges <- all.granges[order(all.granges$chr, all.granges$pos)]
+            win.num <- sum(all.granges$width.to.prev == 0)
+            all.granges$window.num <- rep(1:win.num, diff(c(which(all.granges$width.to.prev == 0), length(all.granges$width.to.prev)+1)))
+        }
+        all.granges
+    }
+}
+
+t.10X10.CV <- function(GenomicMethylSet, groups, pos.val, neg.val,
+                       use.value.dmr=c("p", "q"), p.q.thresh.dmr=0.01, mean.diff.thresh.dmr=0.3, span=3){
+    final.list <- list()
+    for (i in 1:10){
+        set.seed(i)
+        dmps <- t.cross.10X.dmp.list(GenomicMethylSet, groups, pos.val, neg.val)
+        dmrs <- t.cross.10X.dmr.list(dmps, use.value.dmr=use.value.dmr, p.q.thresh.dmr=p.q.thresh.dmr,
+                                     mean.diff.thresh.dmr=mean.diff.thresh.dmr, span=span)
+        final.list[[i]] <- dmrs
+    }
+    final.list
+}
+
+my.dmp.finder.cross.val <- function(betas, group.pos, group.neg){
+    t.list <- lapply(1:nrow(betas), function(r) {
+                                                 my.t <- t.test(x = betas[r,group.neg], y = betas[r,group.pos], alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95)
+                                                 means <- unname(my.t$estimate)
+                                                 c(group.pos.mean=means[2], group.neg.means=means[1],
+                                                 abs.mean.diff=abs(means[2]-means[1]),
+                                                 mean.diff=means[2]-means[1],
+                                                 p.value=my.t$p.value)
+                                                })
+    t.df<- as.data.frame(do.call("rbind", t.list))
+    t.df$q.value <- qvalue(t.df[,"p.value"])$qvalues
+    return(t.df)
+}
+
+t.cross.10X.dmr.list <- function(dmp.list, use.value.dmr=c("p", "q"), p.q.thresh.dmr=0.01, mean.diff.thresh.dmr=0.3, span=3) {
+    dmr.list <- lapply(dmp.list, function(x) my.dmr.finder(x, use.value.dmr=use.value.dmr, p.q.thresh.dmr=p.q.thresh.dmr,
+                                                           mean.diff.thresh.dmr=mean.diff.thresh.dmr, span=span,
+                                                           merge.tables=TRUE))
+    dmr.list
+}
+
+t.cross.10X.dmp.list <- function(GenomicMethylSet, groups, pos.val, neg.val, folds = 10){
+    require(qvalue)
+    require(minfi)
+    stopifnot(pos.val %in% groups, neg.val %in% groups)
+    betas <- getBeta(GenomicMethylSet)
+    anno <- getAnnotation(GenomicMethylSet)
+    locs <- granges(GenomicMethylSet)
+    mcols(locs) <- anno[, c(4, 1:2)]
+    locs$str <- anno$strand
+    locs$dist.to.next <- distance(locs, c(locs[-1], locs[1]))
+    locs$dist.to.prev <- distance(locs, c(locs[length(locs)], locs[-length(locs)]))
+    locs$singleton <- (locs$dist.to.next > 500 | is.na(locs$dist.to.next)) & (locs$dist.to.prev > 500 | is.na(locs$dist.to.prev))
+    mcols(locs) <- cbind(mcols(locs), anno[,c(19,18,24,26,25,32)])
+    my.folds <- createFolds(groups, k=folds, returnTrain=TRUE); print(my.folds)
+    dmp.list <- lapply(my.folds, function(x){
+                                 fold.vals <- groups[x]
+                                 fold.locs <- locs
+                                 #fold.gset <- GenomicMethylSet[,x]
+                                 pos.indx <- x[which(fold.vals == pos.val)]
+                                 neg.indx <- x[which(fold.vals == neg.val)]
+                                 t.df <- my.dmp.finder.cross.val(betas, pos.indx, neg.indx)
+                                 mcols(fold.locs) <- cbind(mcols(locs), t.df)
+                                 #print("here now")
+                                 fold.locs
+                                 })
+    dmp.list
+}
+    
+
+########################################################################
+"createFolds" <- 
+  function(y, k = 10, list = TRUE, returnTrain = FALSE) {
+    if(class(y)[1] == "Surv") y <- y[,"time"]
+    if(is.numeric(y)) {
+      ## Group the numeric data based on their magnitudes
+      ## and sample within those groups.
+      
+      ## When the number of samples is low, we may have
+      ## issues further slicing the numeric data into
+      ## groups. The number of groups will depend on the
+      ## ratio of the number of folds to the sample size.
+      ## At most, we will use quantiles. If the sample
+      ## is too small, we just do regular unstratified
+      ## CV
+      cuts <- floor(length(y)/k)
+      if(cuts < 2) cuts <- 2
+      if(cuts > 5) cuts <- 5
+      breaks <- unique(quantile(y, probs = seq(0, 1, length = cuts)))
+      y <- cut(y, breaks, include.lowest = TRUE)
+    }
+    
+    if(k < length(y)) {
+      ## reset levels so that the possible levels and 
+      ## the levels in the vector are the same
+      y <- factor(as.character(y))
+      numInClass <- table(y)
+      foldVector <- vector(mode = "integer", length(y))
+      
+      ## For each class, balance the fold allocation as far 
+      ## as possible, then resample the remainder.
+      ## The final assignment of folds is also randomized. 
+      for(i in 1:length(numInClass)) {
+        ## create a vector of integers from 1:k as many times as possible without 
+        ## going over the number of samples in the class. Note that if the number 
+        ## of samples in a class is less than k, nothing is producd here.
+        min_reps <- numInClass[i] %/% k
+        if(min_reps > 0) {
+          spares <- numInClass[i] %% k
+          seqVector <- rep(1:k, min_reps)
+          ## add enough random integers to get  length(seqVector) == numInClass[i]
+          if(spares > 0) seqVector <- c(seqVector, sample(1:k, spares))
+          ## shuffle the integers for fold assignment and assign to this classes's data
+          foldVector[which(y == names(numInClass)[i])] <- sample(seqVector)
+        } else {
+          ## Here there are less records in the class than unique folds so
+          ## randomly sprinkle them into folds. 
+          foldVector[which(y == names(numInClass)[i])] <- sample(1:k, size = numInClass[i])
+        }  
+      }
+    } else foldVector <- seq(along = y)
+    
+    if(list) {
+      out <- split(seq(along = y), foldVector)
+      names(out) <- paste("Fold", gsub(" ", "0", format(seq(along = out))), sep = "")
+      if(returnTrain) out <- lapply(out, function(data, y) y[-data], y = seq(along = y))
+    } else out <- foldVector
+    out
+  }
+
+createMultiFolds <- function(y, k = 10, times = 5) {
+  if(class(y)[1] == "Surv") y <- y[,"time"]
+  prettyNums <- paste("Rep", gsub(" ", "0", format(1:times)), sep = "")
+  for(i in 1:times) {
+    tmp <- createFolds(y, k = k, list = TRUE, returnTrain = TRUE)
+    names(tmp) <- paste("Fold",
+                        gsub(" ", "0", format(seq(along = tmp))),
+                        ".",
+                        prettyNums[i],
+                        sep = "")
+    out <- if(i == 1) tmp else c(out, tmp)
+    
+  }
+  out
+}
+    
+    
+    
+    
+    
+#no.sinlge.false <- function(log.vec){
+#	as.numeric( log.vec | (c(1,log.vec[1:(length(log.vec)-1)]) & c(log.vec[2:length(log.vec)],1)) )
+#}
+
